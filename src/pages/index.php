b93932685fb7c6bfbd5f9fd0b53b477cf1658604
Rollfire <>
2023-01-15 19:45:40 +0300

    works need to fix paths of css e t c

--- src/pages/index.php
+++ src/pages/index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styleBase.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/styleBase.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Фрукты и овощи купить</title>
 </head>
 <body>
-    <script type="text/javascript" src="js/counter.js"></script>
+    <script type="text/javascript" src="../js/counter.js"></script>
 
     <?php
-        require_once "php/general_page.php";
+        require_once "../php/general_page.php";
         echo getHeader();
     ?>
 
@@ -330,7 +330,7 @@
     </div>
 
     <?php
-        require_once "php/general_page.php";
+        require_once "../php/general_page.php";
         echo getFooter();
     ?>
 </body>
