db4d1d05a4f89b23918cd9b5e4477b1b6751b605
Rollfire <goronos2000@gmail.com>
2023-01-31 23:03:18 +0300

    promo

--- src/php/general_page.php
+++ src/php/general_page.php
@@ -48,7 +48,7 @@ RIGHT_NAV;
                         <div class="nav-left">
                             <a href="index.php">Главная</a>
                             <a href="catalog.php">Каталог</a>
-                            <a href="about_us.php">О компании</a>
+                            <!-- <a href="about_us.php">О компании</a> -->
                             <a href="delivery.php">Условия доставки</a>
                             <a href="payment.php">Оплата</a>
                             <a href="delivery_to_office.php">Фрукты в офис</a>
