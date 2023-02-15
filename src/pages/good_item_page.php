da5aa4dc37695ae795d3b01922ffd691558c0486
Rollfire <goronos2000@gmail.com>
2023-02-15 23:29:45 +0300

    Update good_item_page.php

--- src/pages/good_item_page.php
+++ src/pages/good_item_page.php
@@ -315,7 +315,7 @@ HIDDEN;
                 </div>
 
                 <div class="flex-row-end">
-                    <textarea maxlength="450" name="comment-area" id="comment-area" cols="40" rows="4"></textarea>
+                    <textarea maxlength="1550" name="comment-area" id="comment-area" cols="40" rows="4"></textarea>
                 </div>
             </form>
 BTN;
